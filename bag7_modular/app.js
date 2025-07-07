450aee55ca0756af8bee40bf914491877d9e4f65
rendykml <rendikamaluddin2018@gmail.com>
2025-07-07 23:52:30 +0700

    push

--- bag7_modular/app.js
+++ bag7_modular/app.js
@@ -2,4 +2,6 @@ import {add, subtract, multiply} from './util.js';
 
 console.log(add(5, 3));    
 console.log(subtract(100, 4)); 
-console.log(multiply(2, 3));    
+console.log(multiply(2, 3));
+
+document.getElementById('output').textContent = 'Modul JavaScript berhasil dijalankan!';
